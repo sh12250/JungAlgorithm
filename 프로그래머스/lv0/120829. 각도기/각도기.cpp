f7163d002d7b78c8e558d6d150c413ad702978f0
sh12250 <99242620+sh12250@users.noreply.github.com>
2023-06-07 09:26:05 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int angle) {
+    if(angle == 180)
+    {
+        return 4;
+    }
+    else if(angle > 90)
+    {
+        return 3;
+    }
+    else if(angle == 90)
+    {
+        return 2;
+    }
+    
+    
+    return 1;
+}
