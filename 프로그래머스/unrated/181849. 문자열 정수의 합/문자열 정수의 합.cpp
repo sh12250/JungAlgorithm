04006161f8aa7f89b9720164023727c308594cd0
sh12250 <99242620+sh12250@users.noreply.github.com>
2023-06-22 20:59:44 +0900

    [unrated] Title: 문자열 정수의 합, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/unrated/181849. 문자열 정수의 합/문자열 정수의 합.cpp
+++ 프로그래머스/unrated/181849. 문자열 정수의 합/문자열 정수의 합.cpp
@@ -5,11 +5,5 @@ using namespace std;
 
 int solution(string num_str) {
     int answer = 0;
-    
-    for(int i = 0; i < num_str.size(); i++)
-    {
-        answer += num_str[i] - 48;
-    }
-    
     return answer;
 }
