afba489e5cf3fb1b258cacbf4dc194a68ab8ba83
sh12250 <99242620+sh12250@users.noreply.github.com>
2023-06-22 21:03:17 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b) {
+    int answer = 0;
+    
+    if(a % 2 != 0 && b % 2 != 0)
+    {
+        answer = a * a + b * b;
+    }
+    else if(a % 2 != 0 || b % 2 != 0)
+    {
+        answer = 2 * (a + b);
+    }
+    else
+    {
+        if(a > b)
+        {
+            answer = a - b;
+        }
+        else 
+        {
+            answer = b - a;
+        }
+    }
+    
+    return answer;
+}
