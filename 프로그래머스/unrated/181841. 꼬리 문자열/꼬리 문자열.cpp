092f080cd59f06d0522875a43d084fa937789ba9
sh12250 <99242620+sh12250@users.noreply.github.com>
2023-07-03 22:00:49 +0900

    [unrated] Title: 꼬리 문자열, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> str_list, string ex) {
+    string answer = "";
+    
+    for(int i = 0; i < str_list.size(); i++)
+    {
+        if(str_list[i].find(ex) == -1)
+        {
+            answer += str_list[i];
+        }
+    }
+    
+    return answer;
+}
