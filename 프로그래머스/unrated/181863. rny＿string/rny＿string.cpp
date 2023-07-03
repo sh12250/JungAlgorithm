9c355549b4b40facc05670361b792f7d9cb153e1
sh12250 <99242620+sh12250@users.noreply.github.com>
2023-07-03 21:48:04 +0900

    [unrated] Title: rny_string, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string rny_string) {
+    for(int i = 0; i < rny_string.size(); i++)
+    {
+        if(rny_string[i] == 'm')
+        {
+            rny_string.replace(i, 1, "rn");
+        }
+    }
+    
+    return rny_string;
+}
