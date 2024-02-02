de8c00374e2fb0cda766b29d651e15306bfc0c90
sh12250 <99242620+sh12250@users.noreply.github.com>
2024-02-02 10:14:51 +0900

    [level 0] Title: 세로 읽기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, int m, int c)
+{
+    string answer = "";
+    
+    for (int i = 0; i < my_string.size() / m; i++)
+    {
+        answer.push_back(my_string[i * m + c - 1]);
+    }
+    
+    return answer;
+}
